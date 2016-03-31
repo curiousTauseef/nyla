41b21ba194904365e3116c8ad180f45c27c350f6
Green <greeng3@usxxgreeng3m4.corp.emc.com>
2016-03-31 13:44:54 -0400

    Added solutions office updates

--- nyla.solutions.global/src/main/java/nyla/solutions/global/io/IO.java
+++ nyla.solutions.global/src/main/java/nyla/solutions/global/io/IO.java
@@ -1326,7 +1326,7 @@ public class IO
 
 		try
 		{
-			writer = new OutputStreamWriter(new FileOutputStream(file), CHARSET.newEncoder());
+			writer = new OutputStreamWriter(new FileOutputStream(file,append), CHARSET.newEncoder());
 
 			writer.write(text);
 		}
